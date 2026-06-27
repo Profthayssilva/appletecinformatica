3378a4e673ce0f99ac9226622415cb6fab599477
Thays Silva <tplsilva05@gmail.com>
2026-06-27 11:18:24 -0300

    Adiciona verificacao do Google Search Console

---
+++ script.js
@@ -0,0 +1,29 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((item) => {
+    const itemTop = item.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (itemTop < windowHeight - 80) {
+      item.classList.add("show");
+    }
+  });
+}
+const albumImages = document.querySelectorAll(".album-img");
+let albumIndex = 0;
+
+function changeAlbumImage() {
+  albumImages[albumIndex].classList.remove("active");
+
+  albumIndex = (albumIndex + 1) % albumImages.length;
+
+  albumImages[albumIndex].classList.add("active");
+}
+
+if (albumImages.length > 0) {
+  setInterval(changeAlbumImage, 3500);
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
