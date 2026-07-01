dc47d497ff4de2bcd7522861801f2d8b471cc90b
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2026-07-01 20:16:43 -0300

    Add load event listener for revealOnScroll
    Added event listener for 'load' to trigger revealOnScroll.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const reveals = document.querySelectorAll(".reveal");
 
 function revealOnScroll() {
@@ -27,34 +27,3 @@ if (albumImages.length > 0) {
 }
 
 window.addEventListener("scroll", revealOnScroll);
-=======
-const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-  reveals.forEach((item) => {
-    const itemTop = item.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (itemTop < windowHeight - 80) {
-      item.classList.add("show");
-    }
-  });
-}
-const albumImages = document.querySelectorAll(".album-img");
-let albumIndex = 0;
-
-function changeAlbumImage() {
-  albumImages[albumIndex].classList.remove("active");
-
-  albumIndex = (albumIndex + 1) % albumImages.length;
-
-  albumImages[albumIndex].classList.add("active");
-}
-
-if (albumImages.length > 0) {
-  setInterval(changeAlbumImage, 3500);
-}
-
-window.addEventListener("scroll", revealOnScroll);
->>>>>>> 1514013f0b776faf6348e6da730bd19833b5e4fb
-window.addEventListener("load", revealOnScroll);
